39042f402c725cb9f99675d3f3a95578446f7406
drankstar <thedrankstar@gmail.com>
2023-10-27 16:55:23 +0300

    hw2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const listImg = galleryItems
+  .map(
+    (img) => `<li class="gallery__item">
+     <a class="gallery__link" href=${img.original}> 
+      <img
+        class="gallery__image"
+        src=${img.preview}
+        data-source=${img.original}
+        alt="${img.description}"
+        />
+     </a>
+  </li> 
+  `
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", listImg);
+gallery.addEventListener("click", handelClick);
+function handelClick(event) {
+  if (event.target.nodeName === "IMG") {
+    event.preventDefault();
+    const src = event.target.dataset.source;
+    const alt = event.target.getAttribute("alt");
+    basicLightbox
+      .create(
+        `
+    <img src=${src} alt=${alt} />
+    `
+      )
+      .show();
+  }
+}
